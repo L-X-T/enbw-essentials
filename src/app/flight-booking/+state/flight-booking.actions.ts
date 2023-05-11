db59ffbd2b9634d4b41928b8a26c01a587f39376
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-05-11 14:21:51 +0200

    Extra: Show loading state and error

--- src/app/flight-booking/+state/flight-booking.actions.ts
+++ src/app/flight-booking/+state/flight-booking.actions.ts
@@ -2,8 +2,8 @@ import { createAction, props } from '@ngrx/store';
 import { Flight } from '../../entities/flight';
 import { HttpErrorResponse } from '@angular/common/http';
 
-export const loadFlights = createAction('[FlightBooking] Load Flights', props<{ from: string; to: string }>());
-export const loadFlightsError = createAction('[FlightBooking] Load Flights Error', props<{ err: HttpErrorResponse }>());
+export const loadFlights = createAction('[FlightBooking] Load Flights', props<{ from: string; to: string; urgent: boolean }>());
+export const loadFlightsError = createAction('[FlightBooking] Load Flights Error', props<{ loadFlightsError: HttpErrorResponse }>());
 export const loadFlightsSuccess = createAction('[FlightBooking] Load Flights Success', props<{ flights: Flight[] }>());
 
 export const updateFlight = createAction('[FlightBooking] Update Flight', props<{ flight: Flight }>());
--- src/app/flight-booking/+state/flight-booking.reducer.ts
+++ src/app/flight-booking/+state/flight-booking.reducer.ts
@@ -1,6 +1,7 @@
 import { createReducer, on } from '@ngrx/store';
 import * as FlightBookingActions from './flight-booking.actions';
 import { Flight } from '../../entities/flight';
+import { HttpErrorResponse } from '@angular/common/http';
 
 export const flightBookingFeatureKey = 'flightBooking';
 
@@ -11,18 +12,30 @@ export interface FlightBookingAppState {
 export interface State {
   flights: Flight[];
   negativeList: number[];
+  isLoadingFlights: boolean;
+  loadFlightsError: HttpErrorResponse | null;
 }
 
 export const initialState: State = {
   flights: [],
-  negativeList: [3, 4, 5]
+  negativeList: [3, 4, 5],
+  isLoadingFlights: false,
+  loadFlightsError: null
 };
 
 export const reducer = createReducer(
   initialState,
 
+  on(FlightBookingActions.loadFlights, (state): State => {
+    return { ...state, flights: [], isLoadingFlights: true, loadFlightsError: null };
+  }),
+
+  on(FlightBookingActions.loadFlightsError, (state, { loadFlightsError }): State => {
+    return { ...state, isLoadingFlights: false, loadFlightsError };
+  }),
+
   on(FlightBookingActions.loadFlightsSuccess, (state, { flights }): State => {
-    return { ...state, flights };
+    return { ...state, flights, isLoadingFlights: false };
   }),
 
   on(FlightBookingActions.updateFlight, (state, { flight }): State => {
