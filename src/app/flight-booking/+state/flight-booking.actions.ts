0d24626d38cd5eeb3aa04a9cfcf9614da32813b7
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-05-11 14:02:36 +0200

    [NgRx] Update a Flight

--- src/app/flight-booking/+state/flight-booking.actions.ts
+++ src/app/flight-booking/+state/flight-booking.actions.ts
@@ -2,3 +2,5 @@ import { createAction, props } from '@ngrx/store';
 import { Flight } from '../../entities/flight';
 
 export const flightsLoaded = createAction('[FlightBooking] Flights Loaded', props<{ flights: Flight[] }>());
+
+export const updateFlight = createAction('[FlightBooking] Update Flight', props<{ flight: Flight }>());
